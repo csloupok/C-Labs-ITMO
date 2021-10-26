685611ae4e0ba5a55b1ae9866c77a7e73f1041ae
Eldar Kasymov <csloupok@gmail.com>
2021-10-26 19:27:05 +0300

    scanf_value added

--- lab-2/lab-2.c
+++ lab-2/lab-2.c
@@ -1,8 +1,15 @@
+//
+// Created by Eldar Kasymov on 21.10.2021.
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 
+// Реализуем функцию выбора максимального элемента
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+// Будем работать в системе по основанию миллиард, т.е. каждый элемент массива содержит сразу 9 цифр.
 const int base = 1000000000;
 
 typedef struct {
@@ -11,12 +18,39 @@ typedef struct {
 } uint1024_t;
 
 void printf_value(uint1024_t x) {
-    if (x.size != 0)
-        printf("%d", x.data[x.size - 1]);
+    // Выводим самый последний элемент вектора (или 0, если вектор пустой).
+    // Затем выводим все оставшиеся элементы вектора, дополняя их нулями до 9 символов.
+    int arr_sz;
+    if (arr_sz != 0)
+        printf("%d", x.data[arr_sz - 1]);
     else
         printf("%d", 0);
-    for (int i = x.size - 2; i >= 0; --i)
+    for (int i = arr_sz - 2; i >= 0; --i)
         printf("%09d", x.data[i]);
+}
+
+void scanf_value(uint1024_t *x) {
+    // Считываем строку и записываем ее в массив чаров
+    char str[1024];
+    scanf("%s", str);
+    int number_len = strlen(str);
+    int inp_size;
+    // Считаем количество ячеек в массиве необходииых для занесения числа
+    if (number_len % 9 == 0)
+        inp_size = number_len / 9;
+    else
+        inp_size = number_len / 9 + 1;
+    uint1024_t inp;
+    inp.size = inp_size;
+    inp.data = malloc(inp_size * sizeof(int32_t));
+    for (int i = number_len, j = 0; i > 0; i -= 9, j++) {
+        str[i] = 0;
+        if (i >= 9)
+            inp.data[j] = str + i - 9;
+        else
+            inp.data[j] = str;
+    }
+    *x = inp;
 }
 
 int main() {
