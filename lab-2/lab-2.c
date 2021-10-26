fe0f30217afc2db7f938f554ea83c6ac198f79e2
Eldar Kasymov <csloupok@gmail.com>
2021-10-26 21:17:13 +0300

    add_op added size variables renamed

--- lab-2/lab-2.c
+++ lab-2/lab-2.c
@@ -5,8 +5,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
 
-// Реализуем функцию выбора максимального элемента
+// Реализуем функцию выбора максимального элемента.
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
 // Будем работать в системе по основанию миллиард, т.е. каждый элемент массива содержит сразу 9 цифр.
@@ -17,46 +18,33 @@ typedef struct {
     int32_t *data;
 } uint1024_t;
 
-
 uint1024_t from_uint(unsigned int x) {
-    int big_int_size;
-    if (x >= base) big_int_size = 2;
-    else big_int_size = 1;
+    int size;
+    if (x >= base) size = 2;
+    else size = 1;
     uint1024_t big_int;
-    big_int.data = malloc(big_int_size * sizeof(int32_t));
-    big_int.size = big_int_size;
+    big_int.data = malloc(size * sizeof(int32_t));
+    big_int.size = size;
     big_int.data[0] = x % base;
-    if (big_int_size > 1)
+    if (size > 1)
         big_int.data[1] = x / base;
     return big_int;
 }
 
-void printf_value(uint1024_t x) {
-    // Выводим самый последний элемент вектора (или 0, если вектор пустой).
-    // Затем выводим все оставшиеся элементы вектора, дополняя их нулями до 9 символов.
-    int arr_sz = x.size;
-    if (arr_sz != 0)
-        printf("%d", x.data[arr_sz - 1]);
-    else
-        printf("%d", 0);
-    for (int i = arr_sz - 2; i >= 0; --i)
-        printf("%09d", x.data[i]);
-}
-
 void scanf_value(uint1024_t *x) {
-    // Считываем строку и записываем ее в массив чаров
+    // Считываем строку и записываем ее в массив чаров.
     char str[1024];
     scanf("%s", str);
     int number_len = strlen(str);
-    int inp_size;
-    // Считаем количество ячеек в массиве необходииых для занесения числа
+    int size;
+    // Считаем количество ячеек в массиве необходииых для занесения числа.
     if (number_len % 9 == 0)
-        inp_size = number_len / 9;
+        size = number_len / 9;
     else
-        inp_size = number_len / 9 + 1;
+        size = number_len / 9 + 1;
     uint1024_t inp;
-    inp.size = inp_size;
-    inp.data = malloc(inp_size * sizeof(int32_t));
+    inp.size = size;
+    inp.data = malloc(size * sizeof(int32_t));
     for (int i = number_len, j = 0; i > 0; i -= 9, j++) {
         str[i] = 0;
         if (i >= 9)
@@ -66,6 +54,42 @@ void scanf_value(uint1024_t *x) {
     }
     *x = inp;
 }
+
+void printf_value(uint1024_t x) {
+    // Выводим самый последний элемент вектора (или 0, если вектор пустой).
+    // Затем выводим все оставшиеся элементы вектора, дополняя их нулями до 9 символов.
+    int size = x.size;
+    if (size != 0)
+        printf("%d", x.data[size - 1]);
+    else
+        printf("%d", 0);
+    for (int i = size - 2; i >= 0; --i)
+        printf("%09d", x.data[i]);
+}
+
+uint1024_t add_op(uint1024_t x, uint1024_t y) {
+    uint1024_t sum;
+    bool overflow = false;
+    int size = max(x.size, y.size);
+    sum.data = malloc(size * sizeof(int32_t));
+    sum.size = size;
+    for (int i = 0; i <= size; i++) {
+        sum.data[i] = x.data[i] + y.data[i] + overflow;
+        if (sum.data[i] >= base)
+            overflow = true;
+        if (overflow)
+            sum.data[i] -= base;
+    }
+
+    if (overflow) {
+        sum.data = realloc(sum.data, (size + 1) * sizeof(int32_t));
+        sum.data[size] = 0;
+        sum.size++;
+    }
+    return sum;
+}
+
+
 
 int main() {
     uint1024_t num1, num2;
