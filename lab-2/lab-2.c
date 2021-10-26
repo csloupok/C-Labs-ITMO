4be3589bcf4c9bfbb0aca1afaa14b3cb65515827
Eldar Kasymov <csloupok@gmail.com>
2021-10-26 17:22:47 +0300

    uint1024_t struct and printf_value added

--- lab-2/lab-2.c
+++ lab-2/lab-2.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+const int base = 1000000000;
+
+typedef struct {
+    size_t size;
+    int32_t *data;
+} uint1024_t;
+
+void printf_value(uint1024_t x) {
+    if (x.size != 0)
+        printf("%d", x.data[x.size - 1]);
+    else
+        printf("%d", 0);
+    for (int i = x.size - 2; i >= 0; --i)
+        printf("%09d", x.data[i]);
+}
 
 int main() {
-    printf("Hello, World!\n");
     return 0;
 }
